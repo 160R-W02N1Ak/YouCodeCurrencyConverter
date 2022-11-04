aa0de04a4b645a6c3bc195a8c0cc5d02020c2d8a
Igor Woźniak <igor91wozniak@gmail.com>
2022-11-04 16:00:57 +0100

    add new functions

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
 {
+
   const welcome = () => {
     console.log("Witam serdecznie wszystkich developerów, którzy tu zaglądają.")
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const currency1Element = document.querySelector(".js-currency1");
+    const currency2Element = document.querySelector(".js-currency2");
+    const resultElement = document.querySelector(".js-result");
+
+    const currency1 = currency1Element.value;
+    const currency2 = currency2Element.value;
+
+    let result = currency1 * currency2;
+    resultElement.innerText = result.toFixed(4);
   };
 
   const init = () => {
     const formElement = document.querySelector(".js-form");
 
-    formElement.addEventListener("submit", (event) => {
-      event.preventDefault();
+    formElement.addEventListener("submit", onFormSubmit);
 
-      const currency1Element = document.querySelector(".js-currency1");
-      const currency2Element = document.querySelector(".js-currency2");
-      const resultElement = document.querySelector(".js-result");
+    welcome();
 
-      const currency1 = currency1Element.value;
-      const currency2 = currency2Element.value;
-
-      let result = currency1 * currency2;
-      resultElement.innerText = result.toFixed(4);
-    });
   };
 
-  welcome();
   init();
+
 }
+
+
+
